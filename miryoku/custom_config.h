9cb2ebfff5356d1d2dff4f816176d60b7b735407
paradoxicator <56213546+paradoxicator@users.noreply.github.com>
2024-06-08 22:53:10 -0500

    Big thumb button backspace and return

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -5,4 +5,4 @@
 &kp Q,             &kp W,             &kp F,             &kp P,             &kp B,             &kp J,             &kp L,             &kp U,             &kp Y,             &kp SQT,           \
 U_MT(LGUI, A),     U_MT(LALT, R),     U_MT(LCTRL, S),    U_MT(LSHFT, T),    &kp G,             &kp M,             U_MT(LSHFT, N),    U_MT(LCTRL, E),    U_MT(LALT, I),     U_MT(LGUI, O),     \
 U_LT(U_BUTTON, Z), U_MT(RALT, X),     &kp C,             &kp D,             &kp V,             &kp K,             &kp H,             &kp COMMA,         U_MT(RALT, DOT),   U_LT(U_BUTTON, SLASH),\
-U_NP,              U_NP,              U_LT(U_FUN, ESC),  U_LT(U_NUM, TAB), U_LT(U_SYM, RET),  U_LT(U_MOUSE, SPACE),U_LT(U_NAV, BSPC),U_LT(U_MEDIA, DEL),U_NP,              U_NP
+U_NP,              U_NP,              U_LT(U_FUN, ESC),  U_LT(U_NUM, TAB), U_LT(U_SYM, BSPC),  U_LT(U_MOUSE, SPACE),U_LT(U_NAV, RET),U_LT(U_MEDIA, DEL),U_NP,              U_NP
